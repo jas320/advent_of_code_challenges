15ae4f2b211c52c55263ff54aa2f3136f2fc1ea5
Joshua Soyke-Pinon <jas320@ic.ac.uk>
2022-12-01 05:41:26 +0000

    [jas320] day1

--- day1.py
+++ day1.py
@@ -1,3 +1,22 @@
 import collections
 import timeit
 import sys
+import os
+
+def loadInput(fileName="input.txt"):
+    __location__ = os.path.realpath(os.path.join(
+        os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(__location__, fileName)) as file:
+        return file.read().splitlines()
+
+inputLines = loadInput()
+b = o = []
+for x in inputLines:
+    if x != '':
+        b.append(int(x))
+    else:
+        o.append(b)
+        b = []
+x = sorted([sum(x) for x in o], reverse=True)
+print(x[0])     # part 1
+print(sum(x[0 : 3]))    # part 2
