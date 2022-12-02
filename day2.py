57a5c6d5e82272e4710be752656c7dc9834efb28
Joshua Soyke-Pinon <jas320@ic.ac.uk>
2022-12-02 06:15:37 +0000

    [jas320] one line python solution

---
+++ day2.py
@@ -0,0 +1,17 @@
+import os
+
+# converts lines to strings
+def loadInput(fileName="input.txt"):
+    __location__ = os.path.realpath(os.path.join(
+        os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(__location__, fileName)) as file:
+        return file.read().splitlines()
+
+res = sum([(((ord(a) - 65) + (ord(b) - 89)) % 3 + 1 + ((ord(b) - 89) + 1) * 3) for a,_,b in loadInput()])
+
+
+
+
+
+
+
