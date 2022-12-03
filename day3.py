6ff3777dd9288060143bbeda56856738d7b8e8a9
Joshua Soyke-Pinon <jas320@ic.ac.uk>
2022-12-03 22:53:00 +0000

    [jas320] generalise intersection n

--- day3.py
+++ day3.py
@@ -7,8 +7,8 @@ inp = loadInput()
 def part1(inp):
     s = 0
     for i in range(0, len(inp), 3):
-        a = inp[i]
-        b = inp[i + 1]
+        a,b = inp[i], inp[i + 1]
+        # b = inp[i + 1]
         c = inp[i + 2]
         res = (set(a).intersection(set(b))).intersection(set(c)) # should only contain 1 item
         res = list(res)[0]
@@ -17,10 +17,22 @@ def part1(inp):
         print(res ,s2)
     return s
 
+def part1a(inp):
+    s = 0
+    n = 3
+    for i in range(0, len(inp), n):
+        prev = set(inp[i])
+        for j in range(1, n):
+            a = set(inp[i + j])
+            prev = a.intersection(prev)
+        res = list(prev)[0]
+        s2 = ord(res) - 96 if res.islower() else ord(res) - 38
+        s += s2
+        print(res ,s2)
+    return s    
     
-    
-    
-print(part1(inp)) 
+# print(part1(inp)) 
+print(part1a(inp)) 
     
     
     
