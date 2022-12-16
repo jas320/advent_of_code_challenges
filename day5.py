f41c7493d43c015643b3e10886ef05b6f78e4092
Joshua Soyke-Pinon <jas320@ic.ac.uk>
2022-12-16 17:53:25 +0000

    [jas320] day5

--- day5.py
+++ day5.py
@@ -1,16 +1,35 @@
 from utils import loadInput
-
+import re
 
 inputLines = loadInput()
 
 # can use either list for amortized (O(1)) from the back or use
 # dequeue import
-
-for line in inputLines:
-    print(line)
-    # if line == "":
-        # break
-# for line in inputLines:
-    # a = line.split(" ")
-    # _,num, _,start,_, end = a
-    
+s = 0
+for i in range(0, len(inputLines)):
+    if inputLines[i][1] == '1':
+        s = i
+        e = int(inputLines[i][-2])
+        print(s, e)
+        break
+g = []
+for j in range(e):
+    b = []
+    for k in range(s - 1, -1, -1):
+        c = inputLines[k][4 * (j+1) - 3]
+        if c != " ":
+            b.append(c)
+    g.append(b)
+print(g)
+for i in range(s + 2, len(inputLines)):
+    a = inputLines[i].split(" ")
+    num,start,end = [int(d) - 1 for d in a if d.isdigit()]
+    if num > 1:
+        g[end] += g[start][-num:]
+        g[start] = g[start][:-num]
+        continue
+    for j in range(num):
+        g[end].append(g[start].pop())
+print(g)
+for x in g:
+    print(x[-1], end="")
