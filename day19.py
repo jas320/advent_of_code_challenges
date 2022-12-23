d2cb5cd36807bdb06afd95bffcac587088f2bc45
Joshua Soyke-Pinon <jas320@ic.ac.uk>
2022-12-23 17:35:49 +0000

    [jas320] renamed files

--- day19.py
+++ day19.py
@@ -1,7 +1,10 @@
 from utils import loadInput, loadeg
 from collections import defaultdict, Counter, deque
 import numpy as np
+import os
 
 
 inputLines = loadInput()
 inputLines = loadeg()
+for i in range(1, 10):
+    os.rename(f"day{i}.py", f"day0{i}.py")
