1d92a6d8a908cfd2b1ac55c7aa0c255d877679ee
jas320 <jas320@ic.ac.uk>
2023-08-20 21:10:00 +0100

    day19 p1

--- 2020/day18.py
+++ 2020/day18.py
@@ -5,5 +5,100 @@ import math
 import sys
 
 inputLines = loadInput()
-inputLines = loadeg()
+# inputLines = loadeg()
+# 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+# ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
 
+
+def check_top(ns, str):
+    if len(ns) > 0:
+        top = ns.pop()
+        ns.append(top)
+        if top == str:
+            return True
+    return False
+
+# two safe numbers of ns stack
+def reduce_normal(ns, ops):
+    op = ops.pop()
+    n2,n1 = ns.pop(), ns.pop()
+    r = n1 * n2 if op == "*" else n1 + n2
+    ns.append(r)
+# reduce down to next '(' called when hitting ")"
+def reduce(ns, ops):
+    hit_open = False
+    nums = []
+    while not hit_open:
+        n = ns.pop()
+        if n == "(":
+            hit_open = True
+        else:
+            nums.append(n)
+    if len(nums) == 2:
+        n1,n2 = nums[0], nums[1]
+        op = ops.pop()
+        r = n1 * n2 if op == "*" else n1 + n2
+        ns.append(r)
+    elif len(nums) == 1:
+        ns.append(nums[0])
+
+
+def next_char_not_plus(line, idx):
+    while idx < len(line):
+        next_char = line[idx]
+        if next_char != " ":
+            if next_char != "+":
+                return True
+            else:
+                return False
+        idx += 1
+    return True
+
+
+
+ns = deque()
+ops = deque()
+t = 0
+for m,l in enumerate(inputLines):
+    # if m == 10:
+    #     break
+    for idx, c in enumerate(l):
+        if c == "(":
+            ns.append(c)
+        elif c == ")":
+            # eval stack up until ()
+            reduce(ns, ops)
+            while len(ns) >= 2 and type(ns[-1]) is int and type(ns[-2]) is int:
+                if len(ops) > 0 and (next_char_not_plus(l, idx + 1) or (ops[-1] == "+")):
+                    reduce_normal(ns, ops)
+                else:
+                    break
+        elif c == "+":
+            ops.append(c)
+        elif c == "*":
+            ops.append(c)
+        elif c.isdigit():
+            open_bracket = check_top(ns, "(")
+            ns.append(int(c))
+            while len(ns) >= 2 and type(ns[-1]) is int and type(ns[-2]) is int:
+                if len(ops) > 0 and (next_char_not_plus(l, idx + 1) or (ops[-1] == "+")):
+                    reduce_normal(ns, ops)
+                else:
+                    break
+        else:
+            continue
+        # if m == 6:
+        # print(ns, ops)
+    while len(ns) > 1:
+        reduce_normal(ns,ops)
+    final = ns.pop()
+    if (len(ops) != 0):
+        print("bad", m)
+        print(ops)
+    print("total:", final)
+    t += final
+print("total", t)
+
+# if hit * check next operator:
+# if is * then collapse
+# if its + then leave on stack
--- 2020/day19.py
+++ 2020/day19.py
@@ -3,6 +3,107 @@ from collections import defaultdict, Counter, deque
 import numpy as np
 import math
 import sys
+import copy
 
+eg = False
 inputLines = loadInput()
-inputLines = loadeg()
+# inputLines = loadeg()
+# eg = True
+# 0: 4 1 5
+# 1: 2 3 | 3 2
+# 2: 4 4 | 5 5
+# 3: 4 5 | 5 4
+# 4: "a"
+# 5: "b"
+# opt 6: 7 | 8
+#
+# ababbb
+# bababa
+# abbbab
+# aaabbb
+# aaaabbb
+
+        
+    
+
+m = dict()
+i = 0
+while i < len(inputLines):
+    l = inputLines[i]
+    if l == "":
+        i += 1
+        break
+    else:
+        if "|" in l:
+            front,back = l.split("|")
+            key,*p1 = front.split(" ")
+            key = int(key[:-1])
+            p1 = [int(x) for x in p1 if x.isnumeric()]
+            p2 = [int(x) for x in back.split(" ") if x.isnumeric()]
+        elif '\"' in l:
+            pass
+        else:
+            key,*p1 = l.split(" ")  
+            key = int(key[:-1])
+            p1 = [int(x) for x in p1 if x.isnumeric()]
+            p2 = []
+        m[key] = (p1,p2)
+    i += 1
+    if not eg:
+        m[35] = ("a",[])
+        m[72] = ("b", [])
+    else:
+        m[4] = ("a",[])
+        m[5] = ("b", [])
+
+
+# generate also possible pairs, then check each one
+# check each as going through
+print(m)
+
+def match_list(m ,rem, nums):
+    for r_num in nums:
+    # can't have run out of chars early
+        if rem == "":
+            return (False, "")
+        is_match, rem = match(m, rem, r_num)
+        if not is_match: # check if ran out of chars early
+            return (False, "")  
+    # print("match_list", rem, nums, "matches")
+    return True, rem
+# returns if those remainding chars match the current rule (without leaving any letters)
+dp = {}
+def match(m ,rem, rule):
+    if rem == '':
+        return True, ""
+    # print(rule)
+    # print(m, ",", rem)
+    p1,p2 = m[rule]
+    if type(p1) is str:
+        if rem[0] == p1:
+            return True, rem[1:]
+        else:
+            return False, ""
+    orig_rem = copy.deepcopy(rem)
+    (is_match, rem) = match_list(m, orig_rem, p1)
+    if not is_match and len(p2) > 0:
+        (is_match, rem) = match_list(m, orig_rem, p2)
+    # print("is_match, rem", is_match, rem)
+    
+    if rule == 0 and rem != "":
+        return False, ""
+    return is_match, rem
+    
+
+
+
+t = 0
+while i < len(inputLines):
+    l = inputLines[i]
+    did_match, result = match(m, l, 0)
+    print(did_match, result)
+    if did_match:
+        t += 1
+    i += 1
+print(t)
+# print(m)
