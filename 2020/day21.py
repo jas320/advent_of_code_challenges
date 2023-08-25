5ca07e5257107552a6044a05112d07f9f58d6c24
Joshua Soyke-Pinon <jas320@ic.ac.uk>
2023-08-25 14:37:33 +0100

    day21

--- 2020/day21.py
+++ 2020/day21.py
@@ -1,8 +1,56 @@
 from utils import loadInput, loadeg
 from collections import defaultdict, Counter, deque
-import numpy as np
 import math
 import sys
 
 inputLines = loadInput()
-inputLines = loadeg()
+# inputLines = loadeg()
+
+# parsing
+all_ing, all_alle = set(), set()
+ing_list = []
+order = []
+for l in inputLines:
+    first, second = l.split(" (contains ")
+    ings = set(first.split(" "))
+    second = second.replace(")","")
+    allegens = set(second.split(", "))
+    order.append((ings, allegens))
+    [ing_list.append(i) for i in ings]
+    # print(ings, allegens)
+    all_ing |= ings
+    all_alle |= allegens
+print(len(all_alle))
+could_be = {x : set(all_ing) for x in all_alle}
+for (ing,alle) in order:
+    for a in alle:
+        # print(a, "intersection of", could_be[a], ing, "is")
+        could_be[a] &= ing
+        # print(could_be[a])
+
+# print(list(could_be.values()))
+left = all_ing - set.union(*list(could_be.values()))
+# print(left)
+c = Counter(ing_list)
+t = sum([c[i] for i in left])
+print(t)
+sortedKeyList = sorted(could_be.keys(), key=lambda s: len(could_be.get(s)))
+res = {}
+finished = False
+
+while not finished:
+    for key in sortedKeyList:
+        if len(could_be[key]) == 1:
+            item = could_be[key].pop()
+            res[item] = key
+            for v in could_be.values():
+                v.discard(item)
+    if len(res) == len(could_be):
+        finished = True
+
+alpha = [k for k, v in sorted(res.items(), key=lambda item: item[1])]
+print(alpha)
+#asner
+
+# ltbj,nrfmm,pvhcsn,jxbnb,chpdjkf,jtqt,zzkq,jqnhd.
+
---
+++ 2020/tips.py
@@ -0,0 +1,55 @@
+# Performance:
+# Deque
+# Adv: for O(1) appending/pop from each end of list (impl as linkedlist), python list is array under hood, so prepening is O(n)
+# Disadv: O(n) instert delete, memory overhead to store pointers O(2n), not designed for sorting/searching, synch issues if used in multi-threaded env
+#
+#
+# Problem solving:
+# if using map/dynamic prog check if map should be local to each recursive call (create empty each time) or global (don't need to pass if python)
+# use deque() 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# Syntax:
+# format(num, "#038b")[2:] - converts number to binary string with leading zeros to "38" width
+# bin(num) - converts integer:100 -> '0b1100100'
+# Bitwise operators - & - AND, | - OR, ^ - XOR, ~ - NOT. a << n, a >> n, left/right shift n bits
+# switch statement in python 3.10 onwards.
+# match lang:
+#     case "Python" | "Not python":
+#         pass
+#     case _:
+#         pass
+# print(f"{ins} is invalid")
+# safe set remove (set.discard(), unsafe remove set.remove() (throws error if not in set))
+# set.union(*list(could_be.values())) if you have dict of set values, then they need to be unioned first
+# sortedKeyList = sorted(could_be.keys(), key=lambda s: len(could_be.get(s)))
+
+
+
+
+
+
+
+
+# String syntax
+# string.split(delimeters)
+# string.strip(",.grt") - txt = ",,,,,rrttgg.....banana....rrr", removes leading/trailing characters, default is whitespace " "
