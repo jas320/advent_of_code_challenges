8fed8941d705916fce0f9f1601a621d8ff6be382
jas320 <jas320@ic.ac.uk>
2023-08-27 20:50:23 +0100

    two days

--- 2020/day23.py
+++ 2020/day23.py
@@ -5,4 +5,54 @@ import math
 import sys
 
 inputLines = loadInput()
-inputLines = loadeg()
+# inputLines = loadeg()
+
+cups = deque([int(x) for x in inputLines[0]])
+final_n = 20
+for i in range(10, final_n):
+    cups.append(i)
+max_n = max(cups)
+cups.rotate(-1)
+# quit()
+print(cups)
+
+# last cup on the right is current cup
+dp = {}
+for x in range(1_000_000):
+    key = (tuple(cups))
+    if key in dp:
+        print("move", x + 1, "curr", curr)
+        print("dp")
+        print(cups)
+        # break
+    else:
+        dp[key] = 1
+    curr = cups[-1]
+    a,b,c = cups.popleft(),cups.popleft(),cups.popleft()
+    # print(cups, [a,b,c])
+    dest = cups[-1] - 1
+    picked = {a,b,c}
+    if dest < 1:
+            dest = max_n
+    while dest in picked:
+        dest -= 1
+        if dest < 1:
+            dest = max_n
+    # print(dest, cups)
+    while cups[-1] != dest:
+        # print(dest, cups)
+        cups.rotate(1)
+    # print(cups)
+    cups.appendleft(c)
+    cups.appendleft(b)
+    cups.appendleft(a)
+    # print(cups)
+    while cups[-2] != curr:
+        cups.rotate(1)
+    # sets up old curr to cups[-2] and new curr cups[-1]
+    # print(cups)    
+while cups[0] != 1:
+    cups.rotate(1)
+# print("".join(map(str,cups)))
+
+    
--- 2020/day24.py
+++ 2020/day24.py
@@ -3,6 +3,70 @@ from collections import defaultdict, Counter, deque
 import numpy as np
 import math
 import sys
+import copy
 
 inputLines = loadInput()
 inputLines = loadeg()
+
+m = {}
+for l in inputLines:
+    i = 0
+    x,y = 0,0
+    while i < len(l):
+        c = l[i]
+        match c:
+            case "e":
+                x += 1
+            case "w":
+                x -= 1
+            case "s":
+                dir = l[i+1]
+                y -= 1
+                x = x - 0.5 if dir == "w" else x + 0.5
+                i += 1
+            case "n":
+                dir = l[i+1]
+                y += 1
+                x = x - 0.5 if dir == "w" else x + 0.5
+                i += 1
+        i += 1
+    if (x,y) in m:
+        m[(x,y)] = (m[(x,y)] + 1) % 2
+    else:
+        m[(x,y)] = 1 # flipped to black
+
+# m = {(0,0) : 0}
+adj = [(-0.5,0),(-0.5,-1),(0.5,-1),(0.5,0),(0.5,1),(-0.5,1)]
+keys = frozenset(m.keys())
+for (x,y) in keys:
+    for (a,b) in adj:
+        if (x+a,y+b) not in m:
+            m[(x+a,y+b)] = 0
+# print(sum(list(m.values())))
+for r in range(1):
+    keys = frozenset(m.keys())
+    for (x,y) in keys:
+        for (a,b) in adj:
+            if (x+a,y+b) not in m:
+                m[(x+a,y+b)] = 0
+    temp_m = copy.deepcopy(m)
+    print(m)
+    for ((x,y), v) in m.items():
+        black_adj = sum([m[(x+a, y+b)] == 1 if (x+a, y+b) in m else 0 for (a,b) in adj])
+        # black_adj_tiles = [((x+a, y+b),m[(x+a, y+b)]) for (a,b) in adj if (x+a, y+b) in m ]
+        # print(black_adj_tiles)
+        if v == 1:
+            if black_adj == 0 or black_adj >= 2:
+                print("flipping to white", ((x,y),v))
+                temp_m[(x,y)] = 0
+        else:
+            # white
+            if black_adj == 2:
+                print("flipping to black", ((x,y),v)) 
+                temp_m[(x,y)] = 1
+
+    m = temp_m
+    
+print(m)
+print(sum(list(m.values())))
+    
